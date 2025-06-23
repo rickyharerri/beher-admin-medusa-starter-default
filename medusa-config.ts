977876872ba336642458f2a9d74b4ed0af72a5c3
Prabhjot Singh Tugger <ricky_4567@yahoo.co.in>
2025-06-23 16:27:36 -0600

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -3,6 +3,9 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+  },
   projectConfig: {
     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     databaseUrl: process.env.DATABASE_URL,
