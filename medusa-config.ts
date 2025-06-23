26a22bd477090446a2e99bacf586fe70c2cd4fdd
Prabhjot Singh Tugger <ricky_4567@yahoo.co.in>
2025-06-23 16:25:50 -0600

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -4,6 +4,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     databaseUrl: process.env.DATABASE_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
